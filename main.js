75d59c36ab0c68923bfaa3b13b847fe8ea8c0897
wnwlals13 <wnwlals13@naver.com>
2021-02-13 20:47:45 +0900

    //finish carrot-game

--- main.js
+++ main.js
@@ -3,117 +3,208 @@
 const startBtn = document.querySelector(".startBtn");
 const remainTime = document.querySelector(".remainTime");
 const clickCount = document.querySelector(".clickCount");
-const playground = document.querySelector(".playground");
-const gaming = document.querySelector(".gaming");
-const afterGame = document.querySelector(".afterGame");
-const afterIcon = document.querySelector(".afterIcon");
-const afterMent = document.querySelector(".afterMent");
-const icon = document.querySelectorAll(".icon");
-const carrot = document.querySelectorAll(".carrot");
-const bug = document.querySelectorAll(".bug");
-let time = 10;
-let count = 0;
-let isStop = Boolean(false);
-const audio = new Audio("sound/bg.mp3");
-const success = new Audio("sound/game_win.mp3");
-const fail = new Audio("sound/alert.wav");
-const carrot_pull = new Audio("sound/carrot_pull.mp3");
-const bug_pull = new Audio("sound/bug_pull.mp3");
+const gameField = document.querySelector(".game_field");
+const fieldRect = gameField.getBoundingClientRect();
+const popUp = document.querySelector(".pop-up");
+const popUpI = document.querySelector(".pop-up_icon");
+const popUpM = document.querySelector(".pop-up_message");
 
-// 1. icons get random position
-function getRandomPosition(icon) {
-  icon.forEach((elem) => {
-    const width = Math.floor(Math.random() * (playground.offsetHeight + 100));
-    const height = Math.floor(Math.random() * (playground.offsetWidth - 100));
+const carrotSize = 80;
+const carrotCount = 10;
+const bugCount = 5;
+const carrotSound = new Audio("/sound/carrot_pull.mp3");
+const bugSound = new Audio("/sound/bug_pull.mp3");
+const bgSound = new Audio("/sound/bg.mp3");
+const alertSound = new Audio("/sound/alert.wav");
+const winSound = new Audio("/sound/game_win.mp3");
 
-    elem.style.position = `absolute`;
-    elem.style.transform = `translate(${width}%,${height}%)`;
-  });
-}
+let decrease = undefined;
+let score = 0;
+let started = false;
+let game_duration = 10;
 
-// 2. when click startBtn, start timer & positioning
+gameField.addEventListener("click", onFieldClick);
 startBtn.addEventListener("click", () => {
-  // audio.play();
-  startGame();
-  getRandomPosition(icon);
+  if (started) {
+    stopGame();
+  } else {
+    startGame();
+  }
 });
 
+popUpI.addEventListener("click", () => {
+  startGame();
+  hidePopUp();
+});
+
+// start game
 function startGame() {
-  startBtn.innerHTML = `<i class="fas fa-stop""></i>`;
-  const stopBtn = document.querySelector(".fa-stop");
-  stopBtn.addEventListener("click", () => {
-    stopGame();
-  });
-  gaming.style.visibility = "visible";
-  const decrease = setInterval(() => {
-    if (time > 0 && !isStop && count < 10) {
-      setTimer(--time);
-      result(time, null);
-    } else {
+  started = true;
+  playSound(bgSound);
+  getRandomPosition();
+  showStopButton();
+  showTimerAndScore();
+  startTimer();
+}
+
+// stop game
+function stopGame() {
+  started = false;
+  stopGameTimer();
+  hideStartBtn();
+  showPopUp("REPLAY ❓");
+  playSound(alertSound);
+  stopSound(bgSound);
+}
+
+//finish gmae
+function finishGame(isWin) {
+  started = false;
+  stopGameTimer();
+  hideStartBtn();
+  if (isWin) {
+    playSound(winSound);
+  } else {
+    playSound(bugSound);
+  }
+  stopSound(bgSound);
+  showPopUp(isWin ? "You Won 🎉" : "You Lost💩");
+}
+
+function showStopButton() {
+  const stopBtn = startBtn.querySelector(".fas");
+  stopBtn.classList.add("fa-stop");
+  stopBtn.classList.remove("fa-play");
+  startBtn.style.visibility = "visible";
+}
+
+function showTimerAndScore() {
+  remainTime.style.visibility = "visible";
+  clickCount.style.visibility = "visible";
+}
+
+function startTimer() {
+  let remaining = game_duration;
+  updateTimerText(remaining);
+  decrease = setInterval(() => {
+    if (remaining <= 0) {
       clearInterval(decrease);
-      gaming.style.visibility = `hidden`;
-      remainTime.innerHTML = `00:10`;
-      clickCount.innerHTML = `0`;
-      startBtn.innerHTML = `<i class="fas fa-play"></i>`;
-      afterGame.style.visibility = "visible";
+      finishGame(carrotCount === score);
+      return;
     }
+    updateTimerText(--remaining);
   }, 1000);
 }
 
-function setTimer(time) {
-  remainTime.innerHTML = `00:0${time}`;
+function updateTimerText(time) {
+  const minutes = Math.floor(time / 60); // 3.2 -> 3
+  const seconds = time % 60;
+  remainTime.innerHTML = `${minutes}:${seconds}`;
 }
 
-// 3. when click stopBtn,stop game
-function stopGame() {
-  console.log("stopBtn clicked");
-  startBtn.innerHTML = `<i class="fas fa-play"></i>`;
-  afterGame.style.visibility = "visible";
-  gaming.style.visibility = `hidden`;
-  remainTime.innerHTML = `00:10`;
-  clickCount.innerHTML = `0`;
+function stopGameTimer() {
+  clearInterval(decrease);
 }
 
-// 4. when click restartBtn, restart game
-afterIcon.addEventListener("click", () => {
-  afterGame.style.visibility = "hidden";
-  time = 10;
-  count = 0;
-  startGame();
-  getRandomPosition(icon);
-  icon.forEach((element) => {
-    element.style.visibility = "visible";
-  });
-});
-
-// 3. when click icons, remove them
-carrot.forEach((element) =>
-  element.addEventListener("click", () => {
-    removeElement(element);
-  })
-);
-
-// 4. when click icons, hide them
-function removeElement(element) {
-  clickCount.innerHTML = `${++count}`;
-  element.style.visibility = "hidden";
-  result(null, count);
+function hideStartBtn() {
+  startBtn.style.visibility = "hidden";
 }
 
-// 5. if you click all carrots, you win. if you or not, you fail.
-function result(time, count) {
-  if ((count != null || time === 0) && count >= 10) {
-    isStop = "";
-    audio.pause();
-    success.play();
-    audio.currentTime = 0;
-    remainTime.innerHTML = `00:10`;
-    clickCount.innerHTML = `${count}`;
-    afterMent.innerHTML = `You Win 🎉`;
-    afterGame.style.visibility = "visible";
-  } else if ((count != null || time === 0) && count < 10) {
-    afterMent.innerHTML = `You Lost 💩`;
-    audio.pause();
-  } else if (time > 0) {
+function showPopUp(text) {
+  popUpM.innerHTML = text;
+  popUp.classList.remove("pop-up--hide");
+}
+
+function hidePopUp() {
+  popUp.classList.add("pop-up--hide");
+}
+
+function onFieldClick(event) {
+  if (!started) {
+    return;
+  }
+  const target = event.target;
+  if (target.matches(".carrot")) {
+    //당근!
+    target.remove();
+    score++;
+    playSound(carrotSound);
+    updateScoreBoard(score);
+    if (score === carrotCount) {
+      finishGame(true);
+    }
+  } else if (target.matches(".bug")) {
+    //벌레!
+
+    finishGame(false);
   }
 }
+
+function playSound(sound) {
+  sound.currentTime = 0;
+  sound.play();
+}
+
+function stopSound(sound) {
+  sound.pause();
+}
+
+function updateScoreBoard(score) {
+  clickCount.innerHTML = carrotCount - score;
+}
+
+// 1. icons get random position
+// make bug and carrot first, and add them to fields. (x, y, width, height) -> getBoundingClientRect();
+function getRandomPosition() {
+  score = 0;
+  gameField.innerHTML = "";
+  clickCount.innerHTML = carrotCount;
+  addItem("carrot", carrotCount, "img/carrot.png");
+  addItem("bug", bugCount, "img/bug.png");
+}
+
+function addItem(className, count, imgPath) {
+  const x1 = 0;
+  const y1 = 0;
+  const x2 = fieldRect.width - carrotSize; //gameField에서 캐럿의 사이즈를 빼줘야 이미지가 화면을 넘어가지 않는다.
+  const y2 = fieldRect.height - carrotSize;
+  for (let i = 0; i < count; i++) {
+    const item = document.createElement("img");
+    item.setAttribute("class", className);
+    item.setAttribute("src", imgPath);
+    item.style.position = "absolute";
+    const x = randomNumber(x1, x2);
+    const y = randomNumber(y1, y2);
+    item.style.left = `${x}px`;
+    item.style.top = `${y}px`;
+    gameField.appendChild(item);
+  }
+}
+
+function randomNumber(min, max) {
+  //included min, excluded max
+  return Math.random() * (max - min) + min;
+}
+
+// 4. when click icons, hide them
+// function removeElement(element) {
+//   clickCount.innerHTML = `${--carrotCount}`;
+//   element.style.visibility = "hidden";
+//   // result(null, carrotCount);
+// }
+// 5. if you click all carrots, you win. if you or not, you fail.
+// function result(time, count) {
+//   if ((count != null || time === 0) && count <= 0) {
+//     isStop = "";
+//     audio.pause();
+//     success.play();
+//     audio.currentTime = 0;
+//     remainTime.innerHTML = `00:10`;
+//     popUpM.innerHTML = `You Win 🎉`;
+//     popUp.classList.remove("pop-up--hide");
+//   } else if ((count != null || time === 0) && count < 10) {
+//     popUpM.innerHTML = `You Lost 💩`;
+//     audio.pause();
+//   } else if (time > 0) {
+//   }
+// }
